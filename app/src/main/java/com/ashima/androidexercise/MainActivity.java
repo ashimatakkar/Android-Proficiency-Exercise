8a6384e996fcf59ab404e080f3192ff2993dbef2
Mayank <apru.takkar@gmail.com>
2018-05-14 13:42:30 +0530

    UI Completed : Added New xml for row and change in main xml

--- app/src/main/java/com/ashima/androidexercise/MainActivity.java
+++ app/src/main/java/com/ashima/androidexercise/MainActivity.java
@@ -2,12 +2,26 @@ package com.ashima.androidexercise;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private RecyclerView recyclerViewFeeds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView()
+    {
+        // inilitize all views by id
+        toolbar=findViewById(R.id.toolbar);
+        recyclerViewFeeds=findViewById(R.id.recyclerViewFeeds);
+        //adding a ToolBar to ActionBar
+        setSupportActionBar(toolbar);
     }
 }
